5f7136fb356fb56f282865276d4492e3078f7ebd
EUNSOL0313 <liat0313@gmail.com>
2024-02-21 00:28:56 +0900

    누나api로변경

--- 뉴스타임즈API/main.js
+++ 뉴스타임즈API/main.js
@@ -1,17 +1,13 @@
-// const API_KEY = `9f293c6208014351b2559d020efc8cc3`
+const API_KEY = `9f293c6208014351b2559d020efc8cc3`
+const url1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+const url2 = 'https://https://dazzling-peony-652d49.netlify.app/top-headlines' //누나api
 let news = [] //전역변수로 선언 후 아래 함수에서는 재할당하기
 
 const getLatestNews = async () => {
    //async-await함수
-   // const url = new URL(
-   //  `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-
-   // )
-   const url = new URL(
-      `https://https://dazzling-peony-652d49.netlify.app/top-headlines?country=kr` //누나api
-   )
-   //  console.log('uuu', url)
-   const response = await fetch(url) //fetch는 url을 호출해서 인터넷을 긁어 올 수 있는 함수 fetch가 끝나면 response를 받을 수있음
+   const requestUrl = new URL(url2)
+   console.log('uuu', requestUrl)
+   const response = await fetch(requestUrl) //fetch는 url을 호출해서 인터넷을 긁어 올 수 있는 함수 fetch가 끝나면 response를 받을 수있음
    const data = await response.json() //json :객체를 텍스트화 시킨 데이터 타입
    //let news = data.article; //함수내에서 먼저 정의하고 확인 후 계속 쓸 변수이기때문에  전역변수로 변경하기
    news = data.article //->여기서는 재할당만해주면 된다.
