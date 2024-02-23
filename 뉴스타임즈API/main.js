f11782f8e75841204558c112ad27ac6ebcf585c4
EUNSOL0313 <liat0313@gmail.com>
2024-02-23 20:30:31 +0900

    뉴스4탄1차

--- 뉴스타임즈API/main.js
+++ 뉴스타임즈API/main.js
@@ -1,6 +1,6 @@
 const API_KEY = `9f293c6208014351b2559d020efc8cc3`
-const url1 = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
-const url2 = `https://dazzling-peony-652d49.netlify.app/top-headlines?country=kr`
+const Url1 = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+const Url2 = `https://dazzling-peony-652d49.netlify.app/top-headlines?country=kr`
 //http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines //누나api
 let newsList = [] //전역변수로 선언 후 아래 함수에서는 재할당하기
 const menus = document.querySelectorAll('.menus button') // 버튼 이벤트 카테고리
@@ -9,44 +9,59 @@ menus.forEach((menu) =>
    menu.addEventListener('click', (event) => getNewsByCategory(event))
 )
 
+//과제 1. 받은 api데이터가 0개라면 화면에 No matches for your search 라는 메세지를 화면에 보여주자
+
+//과제 2. 받은 응답의 코드가 200이 아니라면, (400,401,402 등 ) 받은 에러메세지를 화면에 보여주자.
+
 const fetchDataAndRender = async (requestUrl) => {
    // try/캐치 및 에러 부분 챗gpt도움받아 해결 : 반복되는 코드 한곳에서관리
    try {
       const response = await fetch(requestUrl)
       const data = await response.json()
-      console.log('Data fetched:', data)
+      if (response.status === 200) {
+         if (data.articles.length === 0) {
+            throw new Error('No matches for your search')
+         }
+         newsList = data.articles
+         render()
+      } else {
+         throw new Error(data.message)
+      }
+      //console.log('Data fetched:', data)
       newsList = data.articles
       render()
    } catch (error) {
-      console.error('Error fetching data:', error)
+      console.log('Error fetching data:', error)
+      errorRender(error.message)
    }
 }
 
 const getLatestNews = async () => {
    //async-await함수
-   const requestUrl = new URL(url2)
-   console.log('uuu', requestUrl)
-   const response = await fetch(requestUrl) //fetch는 url을 호출해서 인터넷을 긁어 올 수 있는 함수 fetch가 끝나면 response를 받을 수있음
-   const data = await response.json() //json :객체를 텍스트화 시킨 데이터 타입
+   const requestUrl = new URL(Url1)
+   //console.log('uuu', requestUrl)
+   //const response = await fetch(requestUrl) //fetch는 url을 호출해서 인터넷을 긁어 올 수 있는 함수 fetch가 끝나면 response를 받을 수있음
+   //const data = await response.json() //json :객체를 텍스트화 시킨 데이터 타입
    //let news = data.article; //함수내에서 먼저 정의하고 확인 후 계속 쓸 변수이기때문에  전역변수로 변경하기
-   newsList = data.articles //->여기서는 재할당만해주면 된다.
-   render() //newsList가 확정되는곳
-   console.log('rrr', response) //rrr Promise { <pending> } -> api는 시간이 걸리는 작업
-   // await하면 기다리게 해줌 비동기함수 - async랑 꼭 같이 사용해야함
-   //데이터 네트워크(인터넷)관련은 시간을 좀 기다려줘야함(await)
-   console.log('ddd', data)
-   console.log('aaa', newsList)
+   // newsList = data.articles //->여기서는 재할당만해주면 된다.
+   // render() //newsList가 확정되는곳
+   // console.log('rrr', response) //rrr Promise { <pending> } -> api는 시간이 걸리는 작업
+   // // await하면 기다리게 해줌 비동기함수 - async랑 꼭 같이 사용해야함
+   // //데이터 네트워크(인터넷)관련은 시간을 좀 기다려줘야함(await)
+   // console.log('ddd', data)
+   // console.log('aaa', newsList)
+   fetchDataAndRender(requestUrl)
 }
 
 const getNewsByCategory = async (event) => {
    const category = event.target.textContent.toLowerCase()
    console.log('category', category) //카테고리 버튼 이벤트
-   // const requestUrl = new URL(
-   //    `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
-   // )
    const requestUrl = new URL(
-      `https://dazzling-peony-652d49.netlify.app/top-headlines?country=kr&category=${category}`
+      `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
    )
+   // const requestUrl = new URL(
+   //    `https://dazzling-peony-652d49.netlify.app/top-headlines?country=kr&category=${category}`
+   // )
 
    // const response = await fetch(requestUrl)
    // const data = await response.json()
@@ -58,12 +73,12 @@ const getNewsByCategory = async (event) => {
 const getNewsByKeyword = async () => {
    const keyword = document.querySelector('#search-input').value
    console.log('keyword', keyword)
-   // const requestUrl = new URL(
-   //    `https://newsapi.org/v2/top-headlines?q=${keyword}&country=kr&apiKey=${API_KEY}`
-   // )
    const requestUrl = new URL(
-      `https://dazzling-peony-652d49.netlify.app/top-headlines?q=${keyword}&country=kr`
+      `https://newsapi.org/v2/top-headlines?q=${keyword}&country=kr&apiKey=${API_KEY}`
    )
+   // const requestUrl = new URL(
+   //    `https://dazzling-peony-652d49.netlify.app/top-headlines?q=${keyword}&country=kr`
+   // )
 
    //    const response = await fetch(requestUrl)
    //    const data = await response.json()
@@ -124,6 +139,12 @@ const render = () => {
    //<div>${news.source.name}*${news.publishesAt}</div>
 }
 
+const errorRender = (errorMessage) => {
+   const errorHTML = `<div class="alert alert-danger" role="alert">
+  ${errorMessage}
+</div> `
+   document.querySelector('#news-board').innerHTML = errorHTML
+}
 getLatestNews()
 
 const openNav = () => {
